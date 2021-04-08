a1b634116a1619d8ad5942ed574868e1343ae739
Muni <kangmoonee@gmail.com>
2021-04-09 07:22:46 +0900

    Make Reset-Button

--- src/app.jsx
+++ src/app.jsx
@@ -40,6 +40,13 @@ class App extends Component {
     this.setState({ habits });
   };
 
+  handleReset = () => {
+    const habits = this.state.habits.map((habit) => {
+      habit.count = 0;
+      return habit;
+    });
+    this.setState({ habits });
+  };
   render() {
     return (
       <>
@@ -52,6 +59,7 @@ class App extends Component {
           onDecrement={this.handleDecrement}
           onDelete={this.handleDelete}
           onAdd={this.handleAdd}
+          onReset={this.handleReset}
         />
       </>
     );
